45d8a54dc11ac54beb17f55739f4f4d0da0f077e
neptunejing <jingnanqing@hotmail.com>
2024-12-10 23:05:14 +0200

    fix: change todo-app's routing prefix

--- todoApp/src/main/java/com/nanqing/todoapp/controller/RequestController.java
+++ todoApp/src/main/java/com/nanqing/todoapp/controller/RequestController.java
@@ -1,9 +1,11 @@
 package com.nanqing.todoapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/todo")
 public class RequestController {
     @GetMapping("/port")
     public String getPort() {
