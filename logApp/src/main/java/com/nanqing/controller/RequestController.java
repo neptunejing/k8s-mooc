296543d36a12e5877f88350e8fd938d3cf12453b
neptunejing <jingnanqing@hotmail.com>
2025-01-20 12:10:46 +0200

    feat: namespace and ConfigMap [Ex2.03-2.06]

--- logApp/src/main/java/com/nanqing/controller/RequestController.java
+++ logApp/src/main/java/com/nanqing/controller/RequestController.java
@@ -1,5 +1,7 @@
 package com.nanqing.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,25 +15,33 @@ import java.time.Instant;
 import java.util.HexFormat;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/log")
 public class RequestController {
 
-    private static final String PREFIX;
+    private static final String FILE_PREFIX;
+    private static final String CONFIG_PREFIX;
     private static final String TIMESTAMP_FILE = "timestamp.txt";
     private static final String PINGPONG_COUNT_FILE = "count.txt";
+    private static final String CONFIGMAP_FILE = "ppconfig.txt";
+
+    @Value("${ENV_VAR}")
+    private String myEnvVar;
 
     static {
         if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-            PREFIX = "../files/";
+            FILE_PREFIX = "../files/";
+            CONFIG_PREFIX = "../config/";
         } else {
-            PREFIX = "/app/files/";
+            FILE_PREFIX = "/app/files/";
+            CONFIG_PREFIX = "/app/config/";
         }
     }
 
     @GetMapping("/status")
     public String getCurrentStatus() {
-        String filename = PREFIX + TIMESTAMP_FILE;
+        String filename = FILE_PREFIX + TIMESTAMP_FILE;
         StringBuilder response = new StringBuilder();
         StringBuilder content = new StringBuilder();
 
@@ -54,9 +64,23 @@ public class RequestController {
 
     @GetMapping("/ping-pong-count")
     public String pingPongCount() {
-        String filename = PREFIX + PINGPONG_COUNT_FILE;
+        String configFileName = CONFIG_PREFIX + CONFIGMAP_FILE;
+        String filename = FILE_PREFIX + PINGPONG_COUNT_FILE;
         StringBuilder response = new StringBuilder();
         StringBuilder content = new StringBuilder();
+
+        try {
+            try (BufferedReader reader = new BufferedReader(new FileReader(configFileName))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+                response.append("file Content: ").append(content).append("<br>");
+                response.append("env variable: ").append(myEnvVar).append("<br>");
+            }
+        } catch (IOException e) {
+            log.error("Print configmap volume failed. {}", e.getMessage());
+        }
 
         try {
             try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
@@ -67,12 +91,12 @@ public class RequestController {
                 String timestamp = Instant.now().toString();
                 String hash = calculateMD5Hash(content.toString());
                 response.append(timestamp).append(": ").append(hash).append("<br>");
-                response.append("Ping / Pongs: " + content);
+                response.append("Ping / Pongs: ").append(content);
             }
         } catch (IOException | NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return "Error reading or hashing file: " + e.getMessage();
+            log.error("Print log file failed. {}", e.getMessage());
         }
+
         return response.toString();
     }
 
